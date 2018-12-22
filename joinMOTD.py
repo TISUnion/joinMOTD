2fdacc6ef3d9f76443d748ac56a32da5e220db3c
chino_desu <33673786+kafuuchino-desu@users.noreply.github.com>
2018-12-22 20:49:41 +0800

    init

---
+++ joinMOTD.py
@@ -0,0 +1,13 @@
+﻿from daycount import getday
+
+msgstart = '=====Welcome back to TIS====='
+servername = 'TIS'
+msgend = ''
+
+def onPlayerJoin(server, player):
+  msg = ''
+  msg += msgstart + '\n'
+  msg += '今天是' + servername + '开服的第' + getday() + '天\n'
+  msg += msgend
+  for line in msg.splitlines():
+    server.tell(player, line)
