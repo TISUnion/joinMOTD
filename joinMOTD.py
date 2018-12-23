93713cdc1423ec0728993e4e77cdb10d312db224
chino_desu <33673786+kafuuchino-desu@users.noreply.github.com>
2018-12-23 12:38:19 +0800

    Update joinMOTD.py

--- joinMOTD.py
+++ joinMOTD.py
@@ -1,4 +1,4 @@
-﻿from daycount import getday
+from daycount import getday
 
 msgstart = '=====Welcome back to TIS====='
 servername = 'TIS'
@@ -8,6 +8,7 @@ def onPlayerJoin(server, player):
   msg = ''
   msg += msgstart + '\n'
   msg += '今天是' + servername + '开服的第' + getday() + '天\n'
-  msg += msgend
   for line in msg.splitlines():
     server.tell(player, line)
+  server.execute('tellraw ' + player + '{"text":"点击此处进入创造服","clickEvent":{"action":"run_command","vlaue"："/server creative"}}')
+  server.execute('tellraw ' + player + '{"text":"点击此处列出任务列表","clickEvent":{"action":"run_command","vlaue"："!!task list"}}')
