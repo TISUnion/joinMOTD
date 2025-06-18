8285b72c971412d0d17d5d23913d506ddc8dba31
Fallen_Breath <nellaforax@outlook.com>
2025-06-19 02:27:13 +0800

    added config option `auto_reload_interval`

--- join_motd/__init__.py
+++ join_motd/__init__.py
@@ -1,6 +1,8 @@
 import collections
 import json
 import os
+import random
+import threading
 from datetime import datetime
 from typing import List, Optional, Callable, Any, Union, Dict
 
@@ -36,11 +38,13 @@ class Config(Serializable):
 		'day_count_reforged',
 		'daycount_nbt'
 	]
+	auto_reload_interval: Optional[float] = None
 
 
 Prefix = '!!joinMOTD'
 config: Config
 ConfigFilePath = os.path.join('config', 'joinMOTD.json')
+UnloadFlag = threading.Event()
 
 
 def get_day(server: ServerInterface) -> str:
@@ -89,18 +93,46 @@ def on_player_joined(server: ServerInterface, player, info):
 
 
 def on_load(server: PluginServerInterface, old):
-	global config
-	config = server.load_config_simple(file_name=ConfigFilePath, in_data_folder=False, target_class=Config)
-	if config.commonJsonDataPath is not None:
-		try:
-			with open(config.commonJsonDataPath, 'r', encoding='utf8') as f:
-				common_config_json = json.load(f)
-
-			new_config_json = config.serialize()
-			new_config_json.update(common_config_json)
-			config = Config.deserialize(new_config_json)
-		except Exception as e:
-			server.logger.error('Load common config json from {!r} failed: {}'.format(config.commonJsonDataPath, e))
-
+	load_config(server, echo_in_console=True)
 	server.register_help_message(Prefix, '显示欢迎消息')
 	server.register_command(Literal(Prefix).runs(lambda src: display_motd(src.get_server(), src.reply)))
+
+	def is_auto_reload_enabled() -> bool:
+		return config.auto_reload_interval is not None and config.auto_reload_interval
+
+	def get_auto_reload_interval() -> float:
+		return max(1.0, config.auto_reload_interval + random.random() * 0.2 - 0.1)  # +- 0.1s jitter
+
+	if is_auto_reload_enabled():
+		@new_thread('JoinMOTDAutoReload')
+		def auto_reload():
+			server.logger.debug('AutoReload thread start')
+			while is_auto_reload_enabled() and UnloadFlag.wait(get_auto_reload_interval()) is False:
+				server.logger.debug('Reloading config')
+				load_config(server, echo_in_console=False)
+			server.logger.debug('AutoReload thread end')
+
+		auto_reload()
+
+
+def on_unload(server: PluginServerInterface):
+	UnloadFlag.set()
+
+
+def load_config(server: PluginServerInterface, echo_in_console: bool):
+	new_config = server.load_config_simple(file_name=ConfigFilePath, in_data_folder=False, target_class=Config, echo_in_console=echo_in_console)
+	common_json_path = new_config.commonJsonDataPath
+	if common_json_path is not None:
+		server.logger.debug('Loading common config from {!r}'.format(common_json_path))
+		try:
+			with open(common_json_path, 'r', encoding='utf8') as f:
+				common_config_json = json.load(f)
+
+			new_config_json = new_config.serialize()
+			new_config_json.update(common_config_json)
+			new_config = Config.deserialize(new_config_json)
+		except Exception as e:
+			server.logger.error('Load common config json from {!r} failed: {}'.format(common_json_path, e))
+
+	global config
+	config = new_config
